9c6c59bf7c62e457b44a356a4b2cbc8d9f5a5245
EC2 Default User <ec2-user@ip-172-31-91-103.ec2.internal>
2021-12-18 15:33:17 +0000

    [12/19]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,11 @@
 class BooksController < ApplicationController
   def index
-
-  end
-
-  def new
     @books = Book.all
-    @book = Book.new(book_params)
+    @book = Book.new
   end
 
-   def show
-   @book = Book.find(params[:id])
-   end
-
-
-  def create
-    if @books.save
-      redirect_to show_book_path(@book.id)
-    else
-      render :new
-    end
+  def show
+    @book = Book.find(params[:id])
   end
 
   def edit
@@ -26,19 +13,34 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+  book = Book.find(params[:id])
+  if book.update(book_params)
+  flash[:notice] = 'successfully'
+  redirect_to book_path(book)
+  end
   end
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to new_book_path
+  book = Book.find(params[:id])
+   if book.destroy
+    flash[:notice] = 'successfully'
+  redirect_to books_path(book.id)
+   end
+  end
+
+  def create
+     book = Book.new(book_params)
+    if book.save
+     flash[:notice] = 'successfully'
+     redirect_to books_path(book.id)
+    else
+      render :new
+    end
+
   end
 
 private
 def book_params
-  params.permit(:title, :body)
+  params.require(:book).permit(:title, :body)
 end
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,22 +1,4 @@
 class HomesController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
-  def new
-  end
-
-  def create
-    @book = Book.new(book_params)
-    if @books.save
-      redirect_to show_book_path(@book.id)
-    else
-      render :new
-    end
-  end
-
-  def edit
+  def top
   end
 end
